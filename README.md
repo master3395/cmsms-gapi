bd38efff800c40c3671d4d4cb53d68d63330edb6
arnoud <arnoud@frontendstudio.com>
2015-11-26 18:27:02 +0100

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,130 @@
-# gapi
-UDT to display Google Analytics Data on a CMSMS websites
+=====
+Copyrights
+=====
+
+# (c) 2015 by Arnoud van Susteren (arnoud at nospam frontendstudio dot com)
+# under a GNU General Public License
+
+# The 'gapi.class.php' libary is taken from: https://github.com/erebusnz/gapi-google-analytics-php-interface.git
+# Google Analytic API v3, under the Terms and Conditions of Google
+
+=====
+About
+=====
+
+ * With the {gapi} UDT you can display Google Analytics data on your CMS Made Simple website.
+
+ * This code is using the Google Analytics PHP Interface v2.0.1:
+    https://github.com/erebusnz/gapi-google-analytics-php-interface.git
+
+ * For full Google Analytic API v3 documentation and code examples see:
+    https://developers.google.com/analytics/devguides/reporting/core/v3/quickstart/service-php
+
+ * This code implements a filter for the 10 most visited News module articles in the last 30 days,
+   you can adapt the filter properties to suit your own needs:
+   https://github.com/erebusnz/gapi-google-analytics-php-interface/blob/wiki/UsingFilterControl.md
+
+ * This code has caching support is advisable to set a high cache_time for production websites
+
+=====
+Howto
+=====
+
+step 1) Download gapi.php and gapi.class.php to $root_path/lib/gapi/
+        ! adjust the require on top of gapi.php to point to gapi.class.php
+
+step 2) Register application under Google Cloud Console and generate a key.p12 authentication file
+
+ * Before you can use this code you should register this application via Google Cloud Console
+ * Please go to https://github.com/erebusnz/gapi-google-analytics-php-interface
+   follow the steps in the bottom of this webpage
+
+   In short:
+
+ * Google Cloud Console (generate a key.p12 file): https://console.developers.google.com/
+   - Login to the developer console
+   - Create or Open the project, go to APIs & Auth > Credentials
+   - Click on Create new Client ID, and select Service account and P12 key.
+   - Then click on Create Client ID to download it.
+   - key.p12 is used in the get_gapi_data function of this UDT copy it to $root_path/lib/gapi/ to link it correctly
+
+step 3) Login to the /admin area of your website and add a User Defined Tag
+        (Extensions -> User Defined Tags) with the name "gapi"
+
+step 4) Add the below code (Which is linking to the gapi.class.php and gapi.php files).
+
+* Edit User Defined Tag
+Name: gapi
+Code:
+
+## copy/paste start
+
+global $gCms;
+$config = cmsms()->GetConfig();
+
+$root_path =  $config['root_path'];
+
+$include_class = "$root_path/lib/gapi/gapi.php";
+include($include_class);
+
+## copy/paste end
+
+step 5) Call the gapi UDT by using the correct parameters:
+
+ * ga_profile_id (number:required): Google Analytics ProfileId
+ * ga_email (string:required): Google Analytics Email
+ * max_results (number:optional): Number of page results to fetch
+ * cache_time (number:optional): Number of seconds to cache the Google Analytics .json file
+ * nocache (int:optional): nocache on or off, '0|1'
+ * cache_path (string:optional): where to store the cache file relatively to $config['root_path']
+
+** Example 1: (default)
+
+{gapi ga_profile_id='XXXprofile_id' ga_email='XXXX@developer.gserviceaccount.com'}
+
+or
+
+{gapi ga_profile_id='XXXprofile_id' ga_email='XXXX@developer.gserviceaccount.com' max_results='10' nocache='0' cache_time='14400' cache_path='tmp/cache'}
+
+** Example 2: (non-default)
+
+{gapi ga_profile_id='XXXprofile_id' ga_email='XXXX@developer.gserviceaccount.com' max_results='5' nocache='0' cache_time='600' cache_path='tmp'}
+
+step 6) Print out the Smarty data
+
+ * results, totalResults, pageViews, visits, startDate, endDate
+
+ == copy/paste start
+
+<pre>
+  {$gapi|@print_r}
+
+  {if isset($gapi[0]->visits) }
+
+  {foreach from=$gapi->results item='item' name='loop'}
+    pagePath: {$item->pagePath}
+    pageTitle: {$item->pageTitle}
+    pageId: {$item->pageId}
+    pageViews: {$item->pageViews}
+    visits: {$item->visits}
+  {/foreach}
+
+  {*
+    An example on how to integrate Google Analytics Data with CMSMS modules
+    Don't do intensive operations in foreach loops!
+  *}
+
+  {*news action='detail' articleid=$item->pageId detailtemplate='gapi'*}
+
+  totalResults: {$gapi->totalResults}
+  pageViews: {$gapi->pageViews}
+  visits: {$gapi->visits}
+  startDate: {$gapi->startDate}
+  endDate: {$gapi->endDate}
+
+  {else}
+     Gapi data unavailable!
+  {/if}
+</pre>
+
+== copy/paste end
---
+++ gapi.php
@@ -0,0 +1,152 @@
+<?php
+
+# (c) 2015 by Arnoud van Susteren (arnoud@frontendstudio.com)
+# under a GNU General Public License
+
+# README for documentation and howto!
+
+###############################################################################
+# dependencies
+###############################################################################
+
+if(!is_object(cmsms())) exit;
+
+$config     = cmsms()->GetConfig();
+$sub_smarty = cmsms()->GetSmarty();
+
+require dirname(__FILE__) . '/composer_vendor/gapi-google-analytics-php-interface/gapi.class.php';
+
+###############################################################################
+# config
+###############################################################################
+
+$debug                = 0;
+
+$gapi                 = new stdClass();
+$gapi->dimensions     = array('pagePath', 'pageTitle');
+$gapi->metrics        = array('pageviews', 'visits');
+$gapi->sort_metric    = '-visits';
+$gapi->filter         = 'pagePath =~ ^/verslagen/ && pagePath != /verslagen/ && visits > 1';
+$gapi->start_date     = date('2014-m-d', strtotime("-30 days"));
+$gapi->end_date       = date('2014-m-d', strtotime("-1 days"));
+$gapi->start_index    = 1;
+
+###############################################################################
+# params
+###############################################################################
+
+$gapi->ga_profile_id  = is_int($params['ga_profile_id']) ? $params['ga_profile_id'] : '';
+$gapi->ga_email       = isset($params['ga_email']) ? $params['ga_email'] : '';
+$gapi->max_results    = isset($params['max_results']) ? $params['max_results'] : 10;
+
+$nocache              = is_int($params['nocache']) ? $params['nocache'] : 0;
+$cache_time           = isset($params['cache_time']) ? $params['cache_time'] : 600;
+$cache_path           = isset($params['cache_path']) ? $params['cache_path'] : $config['root_path'].DIRECTORY_SEPARATOR.'tmp/cache';
+$cache_filename       = isset($params['cache_filename']) ? $params['cache_filename'] : 'gapi';
+
+$cache_file           = $cache_path. '/'. $cache_filename.'.json';
+
+if ($gapi->ga_profile_id == '' || $gapi->ga_email == '') {
+  if ($debug) {
+    print "ga_profile_id and ga_email are required!\n";
+  }
+  return;
+}
+
+###############################################################################
+# program
+###############################################################################
+
+$gapi_data = '';
+
+if ($debug) {
+  print $cache_file . "\n";
+}
+
+if (file_exists($cache_file)) {
+  $fh = fopen($cache_file, 'r');
+  $written_time = trim(fgets($fh));
+
+  $cache_time_string = '-'. $cache_time . ' seconds';
+
+  if ($written_time > strtotime($cache_time_string) && $nocache == 0) {
+    if ($debug) { print "read cache file \n"; }
+    $json = fread($fh, filesize($cache_file));
+    fclose($fh);
+  } else {
+    // unlink cache_file
+    fclose($fh);
+    unlink($cache_file);
+
+    if ($debug) { print "get_gapi_data after cache is expired or cache is disabled \n"; }
+    $gapi_data = get_gapi_data($gapi);
+    $json = json_encode($gapi_data);
+
+    $fh = fopen($cache_file, 'w+');
+    fwrite($fh, time() . "\n");
+    fwrite($fh, $json);
+    fclose($fh);
+  }
+
+} else {
+  if ($debug) { print "get_gapi_data cache file does not exist \n"; }
+  $gapi_data = get_gapi_data($gapi);
+  $json = json_encode($gapi_data);
+
+  $fh = fopen($cache_file, 'w+');
+  fwrite($fh, time() . "\n");
+  fwrite($fh, $json);
+  fclose($fh);
+}
+
+$php_data = json_decode($json);
+
+if ($debug) {
+  print_r($php_data);
+}
+
+$sub_smarty->assign('gapi', $php_data);
+
+return;
+
+###############################################################################
+# subroutines
+###############################################################################
+
+function get_gapi_data($gapi) {
+
+  $ga = new gapi($gapi->ga_email, "../../key.p12");
+
+  $ga->requestReportData($gapi->ga_profile_id, $gapi->dimensions, $gapi->metrics, $gapi->sort_metric, $gapi->filter, $gapi->start_date, $gapi->end_date, $gapi->start_index, $gapi->max_results);
+
+  $gapi_data  = new stdClass();
+  $results    = array();
+
+  foreach($ga->getResults() as $result) {
+    $obj              = new stdClass();
+    $obj->pagePath    = $result->getpagePath();
+    $obj->pageTitle   = $result->getpageTitle();
+    $resultList       = explode('/', $result->getpagePath());
+    $obj->pageId      = $resultList[2];
+    $obj->pageViews   = $result->getPageviews();
+    $obj->visits      = $result->getVisits();
+    $results[]        = $obj;
+  }
+
+  $gapi_data->results = $results;
+  $gapi_data->totalResults = $ga->getTotalResults();
+  $gapi_data->pageViews = $ga->getPageviews();
+  $gapi_data->visits = $ga->getVisits();
+  $gapi_data->startDate = $ga->getStartDate();
+  $gapi_data->endDate = $ga->getEndDate();
+
+  return $gapi_data;
+
+}
+
+###############################################################################
+
+#
+# EOF
+#
+?>
